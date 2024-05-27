333d8d90e170424ab20c0b5ccef7577de8fbb2a7
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2024-05-27 23:51:22 +0300

    add heavy armor

---
+++ src/main/kotlin/core/armor/HeavyArmor.kt
@@ -0,0 +1,46 @@
+package io.vbytsyuk.dnd.core.armor
+
+import io.vbytsyuk.dnd.core.armor.Armor.Type.Heavy
+import io.vbytsyuk.dnd.core.money.gold
+import io.vbytsyuk.dnd.core.time.minutes
+import io.vbytsyuk.dnd.core.weight.lb
+
+object RingMail : Armor(
+    type = Heavy(armorClass = 14),
+    cost = 30.gold,
+    weight = 40.lb,
+    hasStealthDisadvantage = true,
+    strengthRequirement = null,
+    timeToPutOn = 10.minutes,
+    timeToRemove = 5.minutes,
+)
+
+object ChainMail : Armor(
+    type = Heavy(armorClass = 16),
+    cost = 75.gold,
+    weight = 55.lb,
+    hasStealthDisadvantage = true,
+    strengthRequirement = 13,
+    timeToPutOn = 10.minutes,
+    timeToRemove = 5.minutes,
+)
+
+object SplintArmor : Armor(
+    type = Heavy(armorClass = 17),
+    cost = 200.gold,
+    weight = 60.lb,
+    hasStealthDisadvantage = true,
+    strengthRequirement = 15,
+    timeToPutOn = 10.minutes,
+    timeToRemove = 5.minutes,
+)
+
+object PlateArmor : Armor(
+    type = Heavy(armorClass = 18),
+    cost = 1500.gold,
+    weight = 65.lb,
+    hasStealthDisadvantage = true,
+    strengthRequirement = 15,
+    timeToPutOn = 10.minutes,
+    timeToRemove = 5.minutes,
+)
