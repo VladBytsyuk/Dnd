a82255a5cb3b05e936f9ed169e2f19d61f859bd6
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2024-05-31 00:30:59 +0300

    change test subject

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,8 @@
 package io.vbytsyuk.dnd
 
 import io.vbytsyuk.dnd.core.*
-import io.vbytsyuk.dnd.core.`class`.Monk
-import io.vbytsyuk.dnd.core.proficiencies.Skills2
+import io.vbytsyuk.dnd.core.`class`.Bard
+import io.vbytsyuk.dnd.core.proficiencies.Skills3
 import io.vbytsyuk.dnd.core.race.*
 import io.vbytsyuk.dnd.core.skills.Skill
 import io.vbytsyuk.dnd.sheet.Sheet
@@ -10,19 +10,20 @@ import io.vbytsyuk.dnd.sheet.print
 
 fun main() {
     val artur = Character(
-        name = "Artur",
-        race = HalfElf(chosenStat1 = StatType.DEX, chosenStat2 = StatType.WIS),
-        `class` = Monk(
-            proficientSkills = Skills2(
-                Skill.Strength.Athletics,
-                Skill.Dexterity.Acrobatics,
-            )
+        name = "Alice",
+        race = Elf.High,
+        `class` = Bard(
+            proficientSkills = Skills3(
+                Skill.Charisma.Intimidation,
+                Skill.Charisma.Performance,
+                Skill.Intelligence.History,
+            ),
         ),
         initialRawStatBlock = StatBlock(
-            strength = 8, dexterity = 15, constitution = 13,
-            intelligence = 10, wisdom = 14, charisma = 12,
+            strength = 8, dexterity = 14, constitution = 13,
+            intelligence = 12, wisdom = 10, charisma = 15,
         ),
-        level = Level(5),
+        level = Level(1),
     )
     val sheet = Sheet(character = artur, playerName = "Artem")
     sheet.print()
