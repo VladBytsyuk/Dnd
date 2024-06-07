d3291c2c83372dd6b47352cf8299529933f49b6d
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2024-06-08 00:49:34 +0300

    add languages

---
+++ src/main/kotlin/core/units/Languages.kt
@@ -0,0 +1,22 @@
+package io.vbytsyuk.dnd.core.units
+
+enum class Languages(val script: Script) {
+    COMMON(script = Script.COMMON),
+    DWARVISH(script = Script.DWARVISH),
+    ELVISH(script = Script.ELVISH),
+    GIANT(script = Script.DWARVISH),
+    GNOMISH(script = Script.DWARVISH),
+    GOBLIN(script = Script.DWARVISH),
+    HALFLING(script = Script.COMMON),
+    ORC(script = Script.DWARVISH),
+    ABYSSAL(script = Script.INFERNAL),
+    CELESTIAL(script = Script.CELESTIAL),
+    DRACONIC(script = Script.DRACONIC),
+    DEEP_SPEECH(script = Script.NONE),
+    INFERNAL(script = Script.INFERNAL),
+    PRIMORDIAL(script = Script.DWARVISH),
+    SYLVAN(script = Script.ELVISH),
+    UNDERCOMMON(script = Script.ELVISH);
+
+    enum class Script { COMMON, DWARVISH, ELVISH, INFERNAL, CELESTIAL, DRACONIC, NONE }
+}
