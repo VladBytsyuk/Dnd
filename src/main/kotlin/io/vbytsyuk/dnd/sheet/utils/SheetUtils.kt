b4b43b34a4e8bf546341d7bebe3e8e898dfb810f
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2024-10-09 17:40:31 +0300

    remove old code

--- src/main/kotlin/io/vbytsyuk/dnd/sheet/utils/SheetUtils.kt
+++
@@ -1,9 +0,0 @@
-package io.vbytsyuk.dnd.sheet.utils
-
-import io.vbytsyuk.dnd.core.sheet.Sheet
-
-
-fun Sheet.Health.toStringOutput() =
-    "$currentHp${if (temporaryHp > 0) "(+$temporaryHp)" else ""}/$maxHp hp " +
-            "(${hitDices.amount}/${hitDices.max} ${hitDices.dice} hit dices)"
-
